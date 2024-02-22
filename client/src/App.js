23be07fd568746845bed2740deebe6a30e2dcb8f
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-02-22 19:09:51 +0530

    Added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
 import NavBar from "./Components/NavBar/NavBar";
-
-function App() {
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Product from "./Pages/Product";
+import ShopCategory from "./Pages/ShopCatergory";
+const App = () => {
   return (
     <div>
-      <NavBar />
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/mens" element={<ShopCategory category="men" />} />
+          <Route path="/women" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/product" element={<Product />}>
+            <Route path=":productId" element={<Product />} />
+          </Route>
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<LoginSignup />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
