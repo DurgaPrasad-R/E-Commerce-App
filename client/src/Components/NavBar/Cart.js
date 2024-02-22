75b7bbc69af6dbea5ae06206818a330142dae235
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-02-22 19:13:01 +0530

    Added Link to Cart

--- client/src/Components/NavBar/Cart.js
+++ client/src/Components/NavBar/Cart.js
@@ -1,14 +1,17 @@
 import cart_icon from "../Assets/cart_icon.png";
+import { Link } from "react-router-dom";
 const Cart = () => {
   return (
     <div className="relative">
-      <img src={cart_icon} alt="cart" className="w-8 h-8" />
+      <Link to="/cart">
+        <img src={cart_icon} alt="cart" className="w-8 h-8" />
 
-      <sup className="absolute top--1 right-[-0.5rem]">
-        <div className="w-5 h-5 flex justify-center items-center bg-red-500 text-white rounded-full">
-          0
-        </div>
-      </sup>
+        <sup className="absolute top--1 right-[-0.5rem]">
+          <div className="w-5 h-5 flex justify-center items-center bg-red-500 text-white rounded-full">
+            0
+          </div>
+        </sup>
+      </Link>
     </div>
   );
 };
