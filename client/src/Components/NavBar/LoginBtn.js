3c2c00ef1c4d4e324f42df5a400a0a04d15fb288
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-02-22 11:08:22 +0530

    LoginBtn Component Added

---
+++ client/src/Components/NavBar/LoginBtn.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <div className="flex items-center gap-11">
+      <button className="w-36 h-14 outline-none border rounded-3xl font-medium cursor-pointer text-orange-500 bg-white active:bg-slate-100">
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Login;
