45f34b692d1b5ee31c6b9bde0d4f79d6250f323b
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-02-24 17:13:56 +0530

    Popular section added!

--- client/src/Components/Popular/Popular.js
+++ client/src/Components/Popular/Popular.js
@@ -2,7 +2,7 @@ import data_product from "../Assets/data";
 import Item from "../Item/Item";
 const Popular = () => {
   return (
-    <div className="flex flex-col items-center gap-3 mt-24 h-[90vh] font-pops">
+    <div className="flex flex-col items-center gap-3 mt-20 font-pops">
       <h1 className="text-3xl font-medium">POPULAR IN WOMEN</h1>
       <hr className="w-52 h-1 rounded-xl bg-gray-600" />
       <div className="mt-12 flex gap-7">
