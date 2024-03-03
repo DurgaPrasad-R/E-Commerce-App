6d2d4df8455c687ce6222c5acbef42be3f9e935c
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-03-03 19:02:18 +0530

    Admin Panel made

--- admin/src/Components/ListProduct/ListProduct.jsx
+++ admin/src/Components/ListProduct/ListProduct.jsx
@@ -15,37 +15,63 @@ const ListProduct = () => {
   useEffect(() => {
     fetchInfo();
   }, []);
+
+  const remove_product = async (id) => {
+    await fetch("http://localhost:4000/removeproduct", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ id: id }),
+    });
+    await fetchInfo();
+  };
   return (
     <div>
-      <h1>All Products List</h1>
+      <h1 className="header">All Products List</h1>
       <div className="list-product">
-        <div className="listproduct-itemfield">
+        <div className="listproduct-format-main">
           <div className="listproduct-item">Image</div>
           <div className="listproduct-item">Product Name</div>
           <div className="listproduct-item">Category</div>
           <div className="listproduct-item">Old Price</div>
           <div className="listproduct-item">New Price</div>
-          <div className="listproduct-item">Action</div>
+          <div className="listproduct-item">Remove</div>
         </div>
-        <div className="listproduct-allproduct">
+        <div className="listproduct-allproducts">
           <hr />
-          {console.log(allProducts)}
           {allProducts.map((product, index) => {
             return (
-              <div key={index} className="listproduct-itemfield">
-                <div className="listproduct-item">
-                  <img src={product.image} alt="product" />
+              <>
+                <div
+                  key={index}
+                  className="listproduct-format-main listproduct-format"
+                >
+                  <div className="listproduct-item">
+                    <img
+                      src={product.image}
+                      alt="product"
+                      className="listproduct-icon"
+                    />
+                  </div>
+                  <div className="listproduct-item">{product.name}</div>
+                  <div className="listproduct-item">{product.category}</div>
+                  <div className="listproduct-item">{product.old_price}</div>
+                  <div className="listproduct-item">{product.new_price}</div>
+                  <div className="listproduct-item">
+                    <img
+                      className="listproduct-remove-icon"
+                      onClick={() => {
+                        remove_product(product.id);
+                      }}
+                      src={cross_icon}
+                      alt="delete"
+                    />
+                  </div>
                 </div>
-                <div className="listproduct-item">{product.name}</div>
-                <div className="listproduct-item">{product.category}</div>
-                <div className="listproduct-item">{product.old_price}</div>
-                <div className="listproduct-item">{product.new_price}</div>
-                <img
-                  className="listproduct-remove-icon"
-                  src={cross_icon}
-                  alt="delete"
-                />
-              </div>
+                <hr />
+              </>
             );
           })}
         </div>
