23b091b1cce221c9b4027d877a4a3500340e8916
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-05-09 18:24:39 +0530

    resolved upload image

--- server/index.js
+++ server/index.js
@@ -37,7 +37,7 @@ app.use("/images", express.static(path.join(__dirname, "uploads/images")));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `${process.env.REACT_APP_BACKEND}/images/${req.file.filename}`,
+    image_url: `https://e-commerce-app-sqbo.onrender.com/images/${req.file.filename}`,
   });
 });
 
